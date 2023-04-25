025f0833e4595039afa0e60b0982c67c716dd13d
Deok Gun Park <intuinno@gmail.com>
2023-04-25 15:38:48 -0500

    Combine pred and video_pred

--- cwvae.py
+++ cwvae.py
@@ -205,7 +205,7 @@ class CWVAE(nn.Module):
         embed, _ = self.hierarchical_encode(obs)
         posteriors, _, _, _, feats = self.hierarchical_observe(embed) 
          
-        initial_decode = self.layers[0]['decoder'](feats[0]).mode()
+        initial_decode = self.layers[0]['decoder'](feats[0]).mode() + 0.5
 
         empty_action = torch.empty(b, num_imagine, 0).to(self.device)
         init_states = []
@@ -219,34 +219,16 @@ class CWVAE(nn.Module):
         recon_loss = nll.mean()
         openl = self.layers[0]['decoder'](feat).mode() + 0.5
         openl = np.clip(to_np(openl), 0, 1)
-        return openl
+        return openl, recon_loss, initial_decode
 
         
     def video_pred(self, data):
-        b, t, c, w, h = data.shape
-        num_initial = self._tmp_abs_factor ** (self._levels-1)
-        num_imagine = t - num_initial
+        num_initial = self._tmp_abs_factor ** (self._levels-1)        
+        openl, recon_loss, initial_decode = self.pred(data, num_initial=num_initial)
         num_gifs = 6
         data = self.preprocess(data)
         truth = data[:num_gifs] + 0.5 
-        obs = data[:,:num_initial]
-        embed, _ = self.hierarchical_encode(obs)
-        posteriors, _, _, _, feats = self.hierarchical_observe(embed) 
-         
-        initial_decode = self.layers[0]['decoder'](feats[0]).mode()[:num_gifs]
-
-        empty_action = torch.empty(b, num_imagine, 0).to(self.device)
-        init_states = []
-        for level in range(self._levels):
-            init = {k: v[:,-1] for k, v in posteriors[level].items()}
-            init_states.append(init)
-
-        feat = self.hierarchical_imagine(empty_action, initial_state=init_states)
-        pred_obs = self.layers[0]['decoder'](feat)
-        nll = -pred_obs.log_prob(data[:, num_initial:])
-        recon_loss = nll.mean()
-        openl = pred_obs.mode()[:num_gifs]
-        model = torch.cat([initial_decode + 0.5,  openl + 0.5], 1)
+        model = torch.cat([initial_decode,  openl], 1)[:num_gifs]
         diff = (model - truth + 1) / 2
         return_video = torch.cat([truth, model, diff], 2) 
         # return_video = (return_video * 255).to(dtype=torch.uint8)
