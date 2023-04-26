ff12516e3ac70e7bc8c3625918e01e99b2f3a694
Deok Gun Park <intuinno@gmail.com>
2023-04-25 19:50:07 -0500

    Added num_worker for train_loader

--- data_loader.py
+++ data_loader.py
@@ -96,7 +96,7 @@ def load_dataset(cfg, **kwargs):
         train_data = torch.utils.data.Subset(train_data, range(200))
         test_data = torch.utils.data.Subset(test_data, range(10))
 
-    train_dataloader = DataLoader(train_data, batch_size=cfg.batch_size, shuffle=True)
+    train_dataloader = DataLoader(train_data, batch_size=cfg.batch_size, shuffle=True, num_workers=4, pin_memory=True)
     test_dataloader = DataLoader(test_data, batch_size=8, shuffle=True)
     return train_dataloader, test_dataloader
 
