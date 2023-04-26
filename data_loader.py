41382633cc75c0d8270382e5a583d9516430eb49
Deok Gun Park <intuinno@gmail.com>
2023-04-26 07:16:08 -0500

    Fixing dataloader

--- data_loader.py
+++ data_loader.py
@@ -1,53 +1,9 @@
 import numpy as np
-from datasets import MovingMNIST, MineRL
+from datasets import MovingMNIST, MineRL, GQNMazes
 from torch.utils.data import DataLoader
 # import tensorflow.compat.v1 as tf
 # import tensorflow_datasets as tfds
 import torch
-
-
-
-class GQNMazes:
-    def __init__(self, batch_size, epochs, train=True, seq_len=None, data_root=None):
-        self._train = train
-        self._batch_size = batch_size
-        self._epochs = epochs
-        self._data_seq_len = 300
-        self._seq_len = seq_len
-        if self._train:
-            ds = tfds.load("gqn_mazes", data_dir=data_root, shuffle_files=True)["train"]
-        else:
-            ds = tfds.load("gqn_mazes", data_dir=data_root, shuffle_files=False)["test"]
-        ds = ds.map(lambda vid: vid["video"]).flat_map(
-            lambda x: tf.data.Dataset.from_tensor_slices(self._process_seq(x))
-        )
-        ds = ds.prefetch(tf.data.experimental.AUTOTUNE)
-        ds = ds.repeat(self._epochs)
-        if self._train:
-            ds = ds.shuffle(10 * self._batch_size)
-        ds = ds.batch(self._batch_size)
-        ds = ds.prefetch(tf.data.experimental.AUTOTUNE)
-
-        self.batch = tf.data.make_one_shot_iterator(ds).get_next()
-
-    def get_batch(self):
-        return self.batch
-
-    def _process_seq(self, seq):
-        if self._seq_len:
-            seq_len_tr = self._data_seq_len - (self._data_seq_len % self._seq_len)
-            seq = seq[:seq_len_tr]
-            seq = tf.reshape(
-                seq,
-                tf.concat(
-                    [[seq_len_tr // self._seq_len, self._seq_len], tf.shape(seq)[1:]],
-                    -1,
-                ),
-            )
-        else:
-            seq = tf.expand_dims(seq, 0)
-        seq = tf.cast(seq, tf.float32) / 255.0
-        return seq
 
 
 
@@ -72,7 +28,6 @@ def load_dataset(cfg, **kwargs):
             train=False,
         )
     elif cfg.dataset == "mazes":
-        import datasets.gqn_mazes
 
         train_data_batch = GQNMazes(
             cfg.batch_size,
@@ -96,7 +51,7 @@ def load_dataset(cfg, **kwargs):
         train_data = torch.utils.data.Subset(train_data, range(200))
         test_data = torch.utils.data.Subset(test_data, range(10))
 
-    train_dataloader = DataLoader(train_data, batch_size=cfg.batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    train_dataloader = DataLoader(train_data, batch_size=cfg.batch_size, shuffle=True, num_workers=2)
     test_dataloader = DataLoader(test_data, batch_size=8, shuffle=True)
     return train_dataloader, test_dataloader
 
