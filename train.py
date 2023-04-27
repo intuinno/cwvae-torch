af1460fe8c43d630daffdac18b914a6f87cc8a3d
Deok Gun Park <intuinno@gmail.com>
2023-04-26 19:29:22 -0500

    Added DDP

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import numpy as np
 import argparse
+import os
 import ruamel.yaml as yaml
 import torch
 import pathlib
@@ -16,47 +17,10 @@ from datetime import datetime
 import pytz
 from prettytable import PrettyTable
 
-import torch.distributed as dist
 import torch.multiprocessing as mp
+from torch.utils.data.distributed import DistributedSampler
 from torch.nn.parallel import DistributedDataParallel as DDP
-import os
-
-def setup(rank, world_size):
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
-
-    # initialize the process group
-    dist.init_process_group("gloo", rank=rank, world_size=world_size)
-
-def cleanup():
-    dist.destroy_process_group()
-    
-def demo_basic(rank, world_size):
-    print(f"Running basic DDP example on rank {rank}.")
-    setup(rank, world_size)
-
-    # create model and move it to GPU with id rank
-    model = ToyModel().to(rank)
-    ddp_model = DDP(model, device_ids=[rank])
-
-    loss_fn = nn.MSELoss()
-    optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
-
-    optimizer.zero_grad()
-    outputs = ddp_model(torch.randn(20, 10))
-    labels = torch.randn(20, 5).to(rank)
-    loss_fn(outputs, labels).backward()
-    optimizer.step()
-
-    cleanup()
-
-def run_demo(demo_fn, world_size):
-    mp.spawn(demo_fn,
-             args=(world_size,),
-             nprocs=world_size,
-             join=True)
-    
-    
+from torch.distributed import init_process_group, destroy_process_group
 
 # device = "cuda" if torch.cuda.is_available() else "cpu"
 # print(f"Using {device} device")
@@ -75,108 +39,147 @@ def count_parameters(model):
     
 
 
+    
+    
+
+def load_train_objs():
+    train_set = MyTrainDataset(2048)  # load your dataset
+    model = torch.nn.Linear(20, 1)  # load your model
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+    return train_set, model, optimizer
+
+
+def prepare_dataloader(dataset: Dataset, batch_size: int):
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        pin_memory=True,
+        shuffle=False,
+        sampler=DistributedSampler(dataset)
+    )
+
+
+def ddp_setup(rank, world_size):
+    """
+    Args:
+        rank: Unique identifier of each process
+        world_size: Total number of processes
+    """
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "12355"
+    init_process_group(backend="nccl", rank=rank, world_size=world_size)
+    
+      
+def main(rank: int, world_size: int, configs):
+    ddp_setup(rank, world_size)
+
+    if rank == 0:
+        tz = pytz.timezone("US/Central")
+        now = datetime.now(tz)
+        date_time = now.strftime("%Y%m%d_%H%M%S")
+        exp_name += date_time
+        
+        # Creating model dir with experiment name.
+        exp_logdir = rootdir / configs.logdir / configs.dataset / exp_name
+        print('Logdir', exp_logdir)
+        exp_logdir.mkdir(parents=True, exist_ok=True)
+
+        # Dumping config.
+        with open(exp_logdir / "config.yml", "w") as f:
+            yaml.dump(configs, f, default_flow_style=False)
+
+        # Build logger
+        logger = tools.Logger(exp_logdir, 0)
+        metrics = {}   
+            
+
+    # Load dataset.
+    train_dataset, val_dataset = load_dataset(configs)
+    train_dataloader = DataLoader(train_dataset, batch_size=configs.batch_size, 
+                                  shuffle=True, pin_memory=True, sampler=DistributedSampler(train_dataset) )
+    val_dataloader = DataLoader(val_dataset, batch_size=8, shuffle=True)
+
+    # Build model
+    model = CWVAE(configs).to(rank)
+    model = DDP(model, device_ids=[rank])
+    count_parameters(model)
+    
+    print(f"========== Using {rank} device ===================")
+
+
+
+    for epoch in range(configs.num_epochs):
+
+        if rank == 0:
+                
+            #Write evaluation summary
+            print(f'======== Epoch {epoch} / {configs.num_epochs} ==========')
+            now = datetime.now(tz)
+            current_time = now.strftime("%H:%M:%S")
+            print("Current Time =", current_time)
+            print (f"Evaluating ... ") 
+            logger.step = epoch
+            if epoch % configs.eval_every == 0:
+                x = next(iter(val_dataloader))
+                openl, recon_loss = model.video_pred(x.to(rank))
+                logger.video('eval_openl', openl)
+                logger.scalar('eval_video_nll', recon_loss)
+                logger.write(fps=True)
+            
+        print(f"Training ...")
+        
+        if rank == 0:
+            dl = tqdm(train_dataloader)
+        else:
+            dl = train_dataloader
+        for i, x in enumerate(dl):
+            x = x.to(rank)
+            met = model.train(x)
+            
+            if rank == 0:
+                for name, values in met.items():
+                    if not name in metrics.keys():
+                        metrics[name] = [values]
+                    else:
+                        metrics[name].append(values)
+        
+        # Write training summary 
+        if rank == 0:
+            for name,values in metrics.items():
+                logger.scalar(name, float(np.mean(values)))
+                metrics[name] = [] 
+            openl, recon_loss = model.video_pred(x)
+            logger.video('train_openl', openl)
+            logger.write(fps=True)
+                
+            # Save Check point
+            if epoch % configs.save_model_every == 0:
+                torch.save(model.state_dict(), exp_logdir / 'latest_model.pt')
+
+    print("Training complete.")
+
+    destroy_process_group()
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--configs', nargs='+', required=True)
-    parser.add_argument('--load_model', type=str)
     args, remaining = parser.parse_known_args()
     rootdir = pathlib.Path(sys.argv[0]).parent
     configs = yaml.safe_load((rootdir / 'configs.yml').read_text())
     
     defaults = {}
+    exp_name = ""
     for name in args.configs:
         defaults.update(configs[name])
+        exp_name = exp_name + name + '_'
     parser = argparse.ArgumentParser()
     for key, value in sorted(defaults.items(), key=lambda x: x[0]):
         arg_type = tools.args_type(value)
         parser.add_argument(f'--{key}', type=arg_type, default=arg_type(value))
     configs = parser.parse_args(remaining)
 
-    exp_name = configs.exp_name + '_'
-
+    world_size = torch.cuda.device_count()
+    mp.spawn(main, args=(world_size, configs), nprocs=world_size)
     
-    tz = pytz.timezone("US/Central")
-    now = datetime.now(tz)
-    date_time = now.strftime("%Y%m%d_%H%M%S")
-    exp_name += date_time
-    
-    # Creating model dir with experiment name.
-    exp_logdir = rootdir / configs.logdir / configs.dataset / exp_name
-    print('Logdir', exp_logdir)
-    exp_logdir.mkdir(parents=True, exist_ok=True)
-
-    # Dumping config.
-    with open(exp_logdir / "config.yml", "w") as f:
-        yaml.dump(configs, f, default_flow_style=False)
-
-    # Load dataset.
-    train_dataset, val_dataset = load_dataset(configs)
-    train_dataloader = DataLoader(train_dataset, batch_size=configs.batch_size, shuffle=True)
-    val_dataloader = DataLoader(val_dataset, batch_size=8, shuffle=True)
-
-    # Build model
-    model = CWVAE(configs).to(configs.device)
-    count_parameters(model)
-    print(f"========== Using {configs.device} device ===================")
-
-    # Load model if args.load_model is not none
-    if args.load_model is not None:
-        model_path = pathlib.Path(args.load_model).expanduser()
-        print(f"========== Loading saved model from {model_path} ===========")
-        checkpoint = torch.load(model_path)
-        model.load_state_dict(checkpoint)
-
-    # Build logger
-    logger = tools.Logger(exp_logdir, 0)
-    metrics = {}
-
-    for epoch in range(configs.num_epochs):
-            
-        #Write evaluation summary
-        print(f'======== Epoch {epoch} / {configs.num_epochs} ==========')
-        now = datetime.now(tz)
-        current_time = now.strftime("%H:%M:%S")
-        print("Current Time =", current_time)
-        print (f"Evaluating ... ") 
-        logger.step = epoch
-        if epoch % configs.eval_every == 0:
-            x = next(iter(val_dataloader))
-            openl, recon_loss = model.video_pred(x.to(configs.device))
-            logger.video('eval_openl', openl)
-            logger.scalar('eval_video_nll', recon_loss)
-            logger.write(fps=True)
-        
-        print(f"Training ...")
-        for i, x in enumerate(tqdm(train_dataloader)):
-            x = x.to(configs.device)
-            met = model.local_train(x)
-            for name, values in met.items():
-                if not name in metrics.keys():
-                    metrics[name] = [values]
-                else:
-                    metrics[name].append(values)
-        
-        # Write training summary 
-        for name,values in metrics.items():
-            logger.scalar(name, float(np.mean(values)))
-            metrics[name] = [] 
-        openl, recon_loss = model.video_pred(x)
-        logger.video('train_openl', openl)
-        logger.write(fps=True)
-        
-        checkpoint = {
-            'epoch': epoch+1, 
-            'model_state_dict': model.state_dict(),
-            # 'logger': logger, 
-        }
-        # Save Check point
-        if epoch % configs.save_model_every == 0:
-            torch.save(checkpoint, exp_logdir / 'latest_checkpoint.pt')
-        
-        if epoch % configs.backup_model_every == 0:
-            torch.save(checkpoint, exp_logdir / f'state_{epoch}.pt')
-
-    print("Training complete.")
-
-
