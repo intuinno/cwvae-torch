85498affe25c192a98b95b41b36019816b5dae9a
Deok Gun Park <intuinno@gmail.com>
2023-05-05 15:43:23 -0500

    Update eval script

--- train.py
+++ train.py
@@ -178,11 +178,17 @@ if __name__ == "__main__":
         logger.step = epoch
         if epoch % configs.eval_every == 0:
             print (f"Evaluating ... ") 
-            x = next(iter(val_dataloader))
-            openl, recon_loss = model.video_pred(x.to(configs.device))
-            logger.video('eval_openl', openl)
-            logger.scalar('eval_video_nll', recon_loss)
-            logger.write(fps=True)
+            recon_loss_list = []
+            for i, x in enumerate(tqdm(val_dataloader)):
+
+                openl, recon_loss = model.video_pred(x.to(configs.device))
+                if i == 0:
+                    logger.video('eval_openl', openl)
+                recon_loss_list.append(recon_loss) 
+            recon_loss_mean = np.mean(recon_loss_list)
+            logger.scalar('eval_video_nll', recon_loss_mean)
+            if epoch == 0:
+                count_parameters(model)
         
         print(f"Training ...")
         for i, x in enumerate(tqdm(train_dataloader)):
